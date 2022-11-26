4e2e4484bf35746de278dcc3d270380187792159
memory <zhangzhigang@pxsemic.com>
2022-11-26 22:21:14 +0800

    1

---
+++ src/views/11.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>sadfasdf</div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const a = ref('asdfsadfasdsssdfdfdfsadfasdf')
+
+console.log(bb)
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,8 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
 import { ref } from 'vue'
-const ss = ref('sdfddddd')
 const f = ref('asdfasdfasdf')
 console.log(f, 'sdfsdaf')
 const aa = '张三dddddd'
-const xx = 'asd'
 console.log(aa, 'asdf', 'sadfasdf', 'asdf')
 </script>
